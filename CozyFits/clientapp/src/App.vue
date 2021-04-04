a606a1ec759eecf89b0813e97c19f83efa25149f
Enoch Johnson <enochjohnsonleon@outlook.com>
2021-04-03 23:02:37 -0400

    Fixed Runtime Application Error

--- CozyFits/clientapp/src/App.vue
+++ CozyFits/clientapp/src/App.vue
@@ -1,67 +1,28 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list dense>
-        <v-list-item to="/">
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item to="chat">
-          <v-list-item-action>
-            <v-icon>mdi-chat</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Chat</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item to="fetchdata">
-          <v-list-item-action>
-            <v-icon>mdi-database</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Fetch Data</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app clipped-left>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>DotNet Vuetify</v-toolbar-title>
-    </v-app-bar>
-
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col class="fill-height">
-            <router-view></router-view>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-
-    <v-footer app>
-      <span>&copy; {{new Date().getYear()+1900}}</span>
-    </v-footer>
-  </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    drawer: null
-  }),
+import HelloWorld from './components/HelloWorld.vue'
 
-  created() {
-    this.$vuetify.theme.dark = true;
-  },
-  props: {}
-};
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
